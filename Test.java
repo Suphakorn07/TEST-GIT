fe503571f08cf67085e4cbc59883ab316d0b52da
suphakorn <suphakorn.l@kkumail.com>
2022-09-02 13:10:34 +0700

    edit3

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("kk");
         System.out.println("suphakorn");
+        System.out.println("gsg");
+        String x = "supahkor";
     }
 }
